c03fb0b7e4b19e6bad9b0e2588f98c101be479c7
John Haskell <john.haskell@gmail.com>
2019-02-02 00:23:04 -0500

    performance improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Skill {
+  name: string;
+  target: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  skills: Skill[];
+
   constructor() { }
 
   ngOnInit() {
+    this.skills = [
+      { name: 'Agile', target: '' },
+      { name: 'AngularJS', target: '' },
+      { name: 'Angular', target: '' },
+      { name: 'ColdFusion (lol)', target: '' },
+      { name: 'CSS3 / SCSS', target: '' },
+      { name: 'Express', target: '' },
+      { name: 'Firebase', target: '' },
+      { name: 'HTML', target: '' },
+      { name: 'JavaScript', target: '' },
+      { name: 'MongoDB', target: '' },
+      { name: 'MySQL', target: '' },
+      { name: 'Node', target: 'https://nodejs.org' },
+      { name: 'PHP', target: '' },
+      { name: 'TypeScript', target: '' },
+    ];
+  }
+
+  openTarget(skill: Skill) {
+    if (skill.target) {
+      window.open(skill.target, '_blank');
+    }
   }
 
 }
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, './functions/dist'),
     filename: '[name].js'
   },
   module: {
